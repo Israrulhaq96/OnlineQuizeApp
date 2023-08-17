4586817ac6eeb80cd0053f85a04776f6a248d773
Israrulhaq96 <104883596+Israrulhaq96@users.noreply.github.com>
2023-08-17 15:22:18 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './quizstyle.css';
+import QuizeComp from "./data/QuizeComp";
+export default function App() {
+
+  return (
+<>
+   <div>
+    <div>
+      <h1 style={{color: "black"}}>Welcome to Online Traffic Quize</h1>
+    </div>
+    <div>
+    <QuizeComp/>
+   </div></div>
+   </>
+  );
+}
+
